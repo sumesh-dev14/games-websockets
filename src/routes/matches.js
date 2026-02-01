977b8bba8a57b17fcbe82940338dcb85f24e4820
sumesh-dev14 <sumesh1614@gmail.com>
2026-02-01 17:28:48 +0530

    websockets server

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -57,6 +57,9 @@ matcheRoutes.post("/", async (req, res) => {
         status: getMatchStatus(parsed.data.startTime, parsed.data.endTime),
       })
       .returning();
+      if(res.app.locals.broadcastMatchCreated){
+        res.app.locals.broadcastMatchCreated(event);
+      }
     return res.status(201).json({
       data: event,
     });
